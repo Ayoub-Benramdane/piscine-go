0799e0231f5320e277715c27ae59006456d52d6e
Ayoub Benramdane <ayoubbenramdane05@gmail.com>
2024-03-06 02:44:38 +0100

    nrune

--- nrune.go
+++ nrune.go
@@ -1,11 +1,10 @@
 package piscine
 
 func NRune(s string, n int) rune {
-	text := []rune(s)
 	if n-1 < 0 {
 		return 0
 	} else if n > len(s) {
 		return 0
 	}
-	return text[n-1]
+	return rune(s[n-1])
 }
