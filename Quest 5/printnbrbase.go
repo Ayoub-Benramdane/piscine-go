4717ffc6a4c6db72bb39a6c04890c360b552f3bd
Ayoub Benramdane <ayoubbenramdane05@gmail.com>
2024-07-05 15:27:08 +0100

    new

--- Quest 5/printnbrbase.go
+++ Quest 5/printnbrbase.go
@@ -1,33 +1,41 @@
 package piscine
 
-import (
-	"github.com/01-edu/z01"
-)
+import "github.com/01-edu/z01"
 
 func PrintNbrBase(nbr int, base string) {
+	str := ""
+	strFinal := ""
+	if handlingError(base) {
+		return
+	}
+	if nbr < 0 {
+		nbr = -nbr
+		strFinal = "-"
+	}
+	for nbr > 0 {
+		str = string(base[nbr%len(base)]) + str
+		nbr /= len(base)
+	}
+	strFinal += str
+	for _, c := range strFinal {
+		z01.PrintRune(c)
+	}
+}
+
+func handlingError(base string) bool {
+	if len(base) < 2 {
+		z01.PrintRune('N')
+		z01.PrintRune('V')
+		return true
+	}
 	for i := 0; i < len(base); i++ {
 		for j := i + 1; j < len(base); j++ {
-			if base[j] == base[i] {
+			if base[j] == base[i] || base[j] == '-' || base[j] == '+' {
 				z01.PrintRune('N')
 				z01.PrintRune('V')
-				return
+				return true
 			}
 		}
 	}
-	str := ""
-	nb := nbr
-	if nbr < 0 {
-		nb = -nbr
-	}
-	for nb > 0 {
-		n := nb % len(base)
-		str = string(base[n]) + str
-		nb /= len(base)
-	}
-	if nbr < 0 {
-		str = "-" + str
-	}
-	for _, c := range str {
-		z01.PrintRune(c)
-	}
+	return false
 }
