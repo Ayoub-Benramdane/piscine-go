51e077cd4fa2cc70f7ccde9deddcfe9f7400698b
Ayoub Benramdane <145620173+Ayoub-Benramdane@users.noreply.github.com>
2024-07-10 14:41:47 +0100

    capitalize.go

--- Quest 5/capitalize.go
+++ Quest 5/capitalize.go
@@ -7,9 +7,7 @@ func isAlphaNum(a rune) bool {
 func Capitalize(s string) string {
 	runes := []rune(s)
 	for i := range runes {
-		if runes[0] >= 'a' && runes[0] <= 'z' {
-			runes[0] -= 32
-		} else if i > 0 && isAlphaNum(runes[i-1]) && runes[i] >= 'a' && runes[i] <= 'z' {
+		if  (i==0 || (i > 0 && isAlphaNum(runes[i-1]))) && runes[i] >= 'a' && runes[i] <= 'z' {
 			runes[i] -= 32
 		} else if i > 0 && !isAlphaNum(runes[i-1]) && runes[i] >= 'A' && runes[i] <= 'Z' {
 			runes[i] += 32
