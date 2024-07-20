8a33d3b72b262ee303eb14cc61b29d17ffa5b862
Ayoub Benramdane <145620173+Ayoub-Benramdane@users.noreply.github.com>
2024-07-20 12:14:36 +0100

    new

--- Quest 3/atoi.go
+++ Quest 3/atoi.go
@@ -4,10 +4,10 @@ func Atoi(s string) int {
 	result := 0
 	t := 1
 	for i, c := range s {
-		if c == '-' && i == 0 {
-			t = -1
-			continue
-		} else if c == '+' && i == 0 {
+		if (c == '-' ||  c == '+') && i == 0 {
+			if c == '-' {
+				t = -1
+			}
 			continue
 		} else if c < '0' || c > '9' {
 			return 0
