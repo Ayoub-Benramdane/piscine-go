e43ff61e6516e17a956a1697fdd5f754ade0e4c3
Ayoub Benramdane <145620173+Ayoub-Benramdane@users.noreply.github.com>
2024-07-20 11:59:22 +0100

    new

--- Quest 2/printcomb2.go
+++ Quest 2/printcomb2.go
@@ -8,18 +8,12 @@ func PrintComb2() {
 			for k := '0'; k <= '9'; k++ {
 				for l := '0'; l <= '9'; l++ {
 					if i < k || j < l && i == k {
-						if i == '9' && j == '8' && k == '9' && l == '9' {
-							z01.PrintRune(i)
-							z01.PrintRune(j)
-							z01.PrintRune(' ')
-							z01.PrintRune(k)
-							z01.PrintRune(l)
-						} else {
-							z01.PrintRune(i)
-							z01.PrintRune(j)
-							z01.PrintRune(' ')
-							z01.PrintRune(k)
-							z01.PrintRune(l)
+						z01.PrintRune(i)
+						z01.PrintRune(j)
+						z01.PrintRune(' ')
+						z01.PrintRune(k)
+						z01.PrintRune(l)
+						if !(i == '9' && j == '8' && k == '9' && l == '9') {
 							z01.PrintRune(',')
 							z01.PrintRune(' ')
 						}
