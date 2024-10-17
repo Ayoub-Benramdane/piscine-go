98dcba9b291d40a51f0eb9c790b4fd5fb8d34fc8
Ayoub Benramdane <145620173+Ayoub-Benramdane@users.noreply.github.com>
2024-10-17 14:48:58 +0100

    Add files via upload

---
+++ deep-copy.js
@@ -0,0 +1,13 @@
+function deepCopy(origine) {
+    if (origine === null || typeof origine !== 'object' || origine instanceof RegExp) {
+        return origine
+    }
+    if (Array.isArray(origine)) return origine.map(item => deepCopy(item))
+    const copiedObject = {}
+    for (const key in origine) {
+        if (origine.hasOwnProperty(key)) {
+            copiedObject[key] = deepCopy(origine[key])
+        }
+    }
+    return copiedObject
+}
